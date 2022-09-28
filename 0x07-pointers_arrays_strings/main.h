4a37b4684cc8e1d65b048852aa4a5cbec427681f
Chrix <chrixiants@gmail.com>
2022-09-27 21:56:24 -0700

    yeah yeah

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * File: holberton.h
+ * File: main.h
  * Auth: Firdaus
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-pointers_arrays_strings directory.
